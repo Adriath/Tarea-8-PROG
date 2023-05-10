df95f916775e0fec9ce5dddb419650dbee30c455
Adriath <adrianarjonabravo@gmail.com>
2023-05-10 14:39:31 +0200

    Creación de la clase Portatil.

---
+++ src/maestre/ordenadores/modelo/Portatil.java
@@ -0,0 +1,18 @@
+
+package maestre.ordenadores.modelo;
+
+/**
+ * Clase que crea objetos de tipo portátil. Hereda de Ordenador.
+ * 
+ * @author Adrián Arjona
+ * @version Mayo 2023
+ */
+public class Portatil extends Ordenador{
+    
+    /* Francisco Adrián Arjona Bravo
+        UNIDAD 8: utilización avanzada de clases.
+    */
+    
+    
+    
+}
