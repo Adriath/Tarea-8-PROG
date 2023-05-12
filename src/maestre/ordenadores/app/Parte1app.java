a4918330d345a775342dab3be3cde9c9c6441766
Adriath <adrianarjonabravo@gmail.com>
2023-05-12 11:36:35 +0200

    Creación del paquete app y de la clase Parte1app.

---
+++ src/maestre/ordenadores/app/Parte1app.java
@@ -0,0 +1,19 @@
+
+package maestre.ordenadores.app;
+
+/**
+ * Primera parte de la aplicación de gestión de ordenadores para la 
+ * tarea 8 de programación.
+ * 
+ * @author Adrián Arjona
+ * @version Mayo 2023
+ */
+public class Parte1app {
+    
+    /* Francisco Adrián Arjona Bravo
+        UNIDAD 8: utilización avanzada de clases.
+    */
+    
+    
+    
+}
